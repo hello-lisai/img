0b3e6facf84608de1edaa6adf47b61a4e75d1ced
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2024-11-27 17:41:17 +0800

    Add files via upload

---
+++ 25/link.js
@@ -0,0 +1,38 @@
+// categories.js
+const categories = [
+    {
+        "categoryName": "社交媒体",
+        "links": [
+            {
+                "name": "示例网站 1",
+                "url": "https://example1.com"
+            },
+            {
+                "name": "示例网站 2",
+                "url": "https://example2.com"
+            }
+        ]
+    },
+    {
+        "categoryName": "学习资源",
+        "links": [
+            {
+                "name": "示例网站 3",
+                "url": "https://example3.com"
+            },
+            {
+                "name": "示例网站 4",
+                "url": "https://example4.com"
+            }
+        ]
+    },
+    {
+        "categoryName": "娱乐",
+        "links": [
+            {
+                "name": "示例网站 5",
+                "url": "https://example5.com"
+            }
+        ]
+    }
+];
